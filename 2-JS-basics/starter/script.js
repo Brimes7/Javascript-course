4e23583282ae67d77b877bfceafae615702d20b3
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-13 22:21:32 -0500

    Added old files for new repo

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,60 @@
+/*
+Variable and Data Types
+console.log("Hello World!!!");
+
+var firstName = "John";
+
+console.log(firstName);
+//String and Variable
+var lastName = "Smith";
+var age = 28; 
+//boolean
+var fullAge = true;
+console.log(fullAge);
+//undefined
+var job;
+console.log(job);
+//now defines
+job = "teacher";
+console.log(job); 
+
+*/
+
+//Variable mutation and type coercion
+
+// var _3years = 3; 
+
+// var firstName = "John";
+// var age = 28;
+
+// console.log(firstName + " " + age);
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// console.log(firstName = " is a " + age + " year old " + job + ". Is he married?"  + isMarried);
+
+// //Variable mutation
+
+// age = "twenty eight";
+// job = "driver";
+// //Gives an alert not a console
+// // alert(firstName = " is a " + age + " year old " + job + ". Is he married?"  + isMarried);
+
+// var lastName = prompt("What is his last Name?");
+// console.log(firstName + " " +lastName)
+
+
+
+//BASIC OPERATORS.
+var year = 2020
+var yearJohn = year-28;
+var yearMark = year-33;
+
+console.log(yearJohn);
+
+console.log(year + 2);
+
+console.log(year / 10);//Logical Variable
+
