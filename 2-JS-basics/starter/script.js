654fea547c80c61a1fc640317685af3910a8f654
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-08-24 21:21:48 -0500

    True and falsy values.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -92,9 +92,9 @@ console.log(job);
 //truthy values:; NOT falsy values
 
 var height;
-height = 23;
+height = 0;
 
-if(height) {
+if(height || height === 0) {
     console.log('Variable is defined');
 } else {
     console.log('Variable has NOT been defined.');
